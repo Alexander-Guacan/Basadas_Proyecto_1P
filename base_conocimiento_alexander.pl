dd4739c13bdcfc7004f0a6096bc0c79119a9cf5e
Alexander-Guacan <adguacan@espe.edu.ec>
2024-06-15 05:25:31 -0500

    agregar megadeth a la base de conocimientos

---
+++ base_conocimiento_alexander.pl
@@ -0,0 +1,30 @@
+% Canción "Rust in Peace... Polaris"
+acordes_verso('megadeth', ['A', 'E', 'D', 'G']).
+acordes_precoro('megadeth', ['F#m', 'Bm', 'G', 'A']).
+acordes_coro('megadeth', ['D', 'A', 'E', 'G']).
+acordes_puente('megadeth', ['Em', 'G', 'D', 'C']).
+
+% Canción "Holy Wars"
+acordes_verso('megadeth', ['E', 'A', 'D', 'G']).
+acordes_precoro('megadeth', ['Em', 'C', 'G', 'D']).
+acordes_coro('megadeth', ['A', 'E', 'F#m', 'D']).
+acordes_puente('megadeth', ['Bm', 'C', 'G', 'A']).
+
+% Canción "Tornado of Souls"
+acordes_verso('megadeth', ['D', 'A', 'G', 'Bm']).
+acordes_precoro('megadeth', ['Em', 'G', 'D', 'A']).
+acordes_coro('megadeth', ['Bm', 'A', 'G', 'D']).
+acordes_puente('megadeth', ['Em', 'G', 'D', 'A']).
+
+% Canción "Hangar 18"
+acordes_verso('megadeth', ['Em', 'A', 'G', 'D']).
+acordes_precoro('megadeth', ['C', 'D', 'G', 'Bm']).
+acordes_coro('megadeth', ['Em', 'G', 'D', 'A']).
+acordes_puente('megadeth', ['Bm', 'A', 'G', 'D']).
+
+% Canción "Sweating Bullets"
+acordes_verso('megadeth', ['E', 'G', 'A', 'D']).
+acordes_precoro('megadeth', ['C', 'G', 'D', 'A']).
+acordes_coro('megadeth', ['E', 'A', 'D', 'G']).
+acordes_puente('megadeth', ['C', 'G', 'D', 'A']).
+
