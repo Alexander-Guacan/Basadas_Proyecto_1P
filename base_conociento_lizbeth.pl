962741deb0027c86b314faed5c7f64f7296fe678
KarlaLizbethCajasRecalde <klcajas1@espe.edu.ec>
2024-06-15 12:53:04 -0500

    agregado2

---
+++ base_conociento_lizbeth.pl
@@ -0,0 +1,132 @@
+% Canción "Bohemian Rhapsody"
+acordes_verso('queen', ['Bb', 'Gm', 'Cm', 'F7']).
+acordes_precoro('queen', ['Eb', 'Ab', 'Eb', 'Abm', 'Cm', 'G', 'Cm', 'G', 'Cm', 'F7', 'Bb', 'Eb', 'Ab', 'D7', 'Gm', 'Bb/D', 'Eb', 'Abm']).
+acordes_coro('queen', ['Am', 'G', 'Am', 'C', 'G', 'C', 'G', 'G', 'Bb', 'G', 'Bb']).
+acordes_puente('queen', ['Eb', 'Bb', 'Eb', 'Bb', 'Eb', 'Bb', 'Eb', 'Ab', 'Eb', 'Bb', 'Eb']).
+
+% Canción "We Will Rock You"
+acordes_verso('queen', ['E', 'A', 'D']).
+acordes_precoro('queen', ['A', 'D', 'E']).
+acordes_coro('queen', ['E', 'A', 'D']).
+acordes_puente('queen', ['A', 'D', 'E']).
+
+% Canción "Radio Ga Ga"
+acordes_verso('queen', ['D', 'G', 'Em', 'A']).
+acordes_precoro('queen', ['Bm', 'A', 'G', 'D']).
+acordes_coro('queen', ['G', 'D', 'Em', 'A']).
+acordes_puente('queen', ['Em', 'C', 'G', 'D']).
+
+% Canción "We Will Rock You"
+acordes_verso('queen', ['D', 'A', 'D', 'A', 'G', 'A']).
+acordes_precoro('queen', ['C', 'G', 'A', 'D']).
+acordes_coro('queen', ['G', 'A', 'G', 'A']).
+acordes_puente('queen', ['C', 'G', 'A', 'D']).
+
+% Canción "Crazy Little Thing Called Love"
+acordes_verso('queen', ['D', 'G', 'C', 'G']).
+acordes_precoro('queen', ['D', 'C', 'G', 'D']).
+acordes_coro('queen', ['G', 'C', 'G']).
+acordes_puente('queen', ['Bb', 'Eb', 'Bb']).
+
+
+% Canción "El Tiempo de Ti"
+acordes_verso('playa limbo', ['Am', 'G', 'F', 'C']).
+acordes_precoro('playa limbo', ['F', 'C', 'G', 'Am']).
+acordes_coro('playa limbo', ['Am', 'G', 'F', 'C']).
+acordes_puente('playa limbo', ['Dm', 'Am', 'G', 'F']).
+
+
+% Canción "Piérdeme el Respeto"
+acordes_verso('playa limbo', ['C', 'Am', 'F', 'G']).
+acordes_precoro('playa limbo', ['F', 'G', 'Am', 'C']).
+acordes_coro('playa limbo', ['C', 'Am', 'F', 'G']).
+acordes_puente('playa limbo', ['F', 'C', 'G', 'Am']).
+
+% Canción "Imaginarte"
+acordes_verso('playa limbo', ['Em', 'C', 'G', 'D']).
+acordes_precoro('playa limbo', ['C', 'D', 'Em', 'G']).
+acordes_coro('playa limbo', ['Em', 'C', 'G', 'D']).
+acordes_puente('playa limbo', ['Am', 'Em', 'D', 'C']).
+
+% Canción "A Dónde Va Nuestro Amor"
+acordes_verso('playa limbo', ['G', 'D', 'Em', 'C']).
+acordes_precoro('playa limbo', ['C', 'G', 'D', 'Em']).
+acordes_coro('playa limbo', ['G', 'D', 'Em', 'C']).
+acordes_puente('playa limbo', ['Am', 'Em', 'D', 'C']).
+
+
+% Canción "Tu Voz"
+acordes_verso('playa limbo', ['G', 'C', 'Em', 'D']).
+acordes_precoro('playa limbo', ['C', 'D', 'G', 'Em']).
+acordes_coro('playa limbo', ['G', 'C', 'Em', 'D']).
+acordes_puente('playa limbo', ['Am', 'Em', 'D', 'C']).
+
+
+% Canción "Amor Prohibido"
+acordes_verso('selena', ['G', 'Em', 'C', 'D']).
+acordes_precoro('selena', ['C', 'D', 'G', 'Em']).
+acordes_coro('selena', ['G', 'Em', 'C', 'D']).
+acordes_puente('selena', ['Em', 'C', 'D', 'G']).
+
+
+% Canción "Como La Flor"
+acordes_verso('selena', ['D', 'G', 'A', 'D']).
+acordes_precoro('selena', ['G', 'A', 'D']).
+acordes_coro('selena', ['D', 'G', 'A', 'D']).
+acordes_puente('selena', ['Bm', 'G', 'A', 'D']).
+
+
+% Canción "Bidi Bidi Bom Bom"
+acordes_verso('selena', ['G', 'C', 'D', 'G']).
+acordes_precoro('selena', ['C', 'D', 'G']).
+acordes_coro('selena', ['G', 'C', 'D', 'G']).
+acordes_puente('selena', ['Em', 'C', 'D', 'G']).
+
+% Canción "No Me Queda Más"
+acordes_verso('selena', ['G', 'Em', 'C', 'D']).
+acordes_precoro('selena', ['C', 'D', 'G', 'Em']).
+acordes_coro('selena', ['G', 'Em', 'C', 'D']).
+acordes_puente('selena', ['Em', 'C', 'D', 'G']).
+
+% Canción "Dreaming of You"
+acordes_verso('selena', ['G', 'D', 'Em', 'C']).
+acordes_precoro('selena', ['C', 'G', 'D', 'Em']).
+acordes_coro('selena', ['G', 'D', 'Em', 'C']).
+acordes_puente('selena', ['Am', 'Em', 'D', 'C']).
+
+% Canción "Sin Despertar"
+acordes_verso('kudai', ['Am', 'F', 'C', 'G']).
+acordes_precoro('kudai', ['F', 'G', 'Am', 'C']).
+acordes_coro('kudai', ['Am', 'F', 'C', 'G']).
+acordes_puente('kudai', ['Dm', 'Am', 'G', 'F']).
+
+% Canción "Escapar"
+acordes_verso('kudai', ['C', 'G', 'Am', 'F']).
+acordes_precoro('kudai', ['F', 'G', 'Am', 'C']).
+acordes_coro('kudai', ['C', 'G', 'Am', 'F']).
+acordes_puente('kudai', ['Dm', 'Am', 'G', 'F']).
+
+% Canción "Déjame Gritar"
+acordes_verso('kudai', ['G', 'D', 'Em', 'C']).
+acordes_precoro('kudai', ['C', 'D', 'G', 'Em']).
+acordes_coro('kudai', ['G', 'D', 'Em', 'C']).
+acordes_puente('kudai', ['Am', 'Em', 'D', 'C']).
+
+% Canción "Nada es Igual"
+acordes_verso('kudai', ['D', 'A', 'Bm', 'G']).
+acordes_precoro('kudai', ['G', 'A', 'D', 'Bm']).
+acordes_coro('kudai', ['D', 'A', 'Bm', 'G']).
+acordes_puente('kudai', ['Em', 'Bm', 'A', 'G']).
+
+% Canción  "Tal Vez"
+acordes_verso('kudai', ['Em', 'C', 'G', 'D']).
+acordes_precoro('kudai', ['C', 'D', 'Em', 'G']).
+acordes_coro('kudai', ['Em', 'C', 'G', 'D']).
+acordes_puente('kudai', ['Am', 'Em', 'D', 'C']).
+
+
+
+
+
+
+
