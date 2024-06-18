111fcba58e2b9f8ccdb8c7e4f78563c5e91800a6
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-06-17 20:57:14 -0500

    Update base_conocimiento.pl

--- base_conocimiento.pl
+++ base_conocimiento.pl
@@ -1,119 +1,83 @@
-% Canción "Enter Sandman"
 acordes_verso('metallica', ['E', 'G', 'F', 'E']).
-acordes_precoro('metallica', ['C', 'D', 'E', 'G']).
-acordes_coro('metallica', ['E', 'G', 'F', 'E']).
-acordes_puente('metallica', ['D', 'E', 'F', 'G']).
-
-% Canción "Master of Puppets"
 acordes_verso('metallica', ['E', 'G', 'A', 'F']).
-acordes_precoro('metallica', ['D', 'C', 'G', 'E']).
-acordes_coro('metallica', ['A', 'G', 'F', 'E']).
-acordes_puente('metallica', ['B', 'C', 'D', 'E']).
-
-% Canción "Nothing Else Matters"
 acordes_verso('metallica', ['Em', 'D', 'C', 'G']).
-acordes_precoro('metallica', ['G', 'B', 'Em', 'D']).
-acordes_coro('metallica', ['C', 'D', 'Em', 'G']).
-acordes_puente('metallica', ['C', 'G', 'D', 'Em']).
-
-% Canción "The Unforgiven"
 acordes_verso('metallica', ['Am', 'G', 'F', 'E']).
-acordes_precoro('metallica', ['C', 'G', 'Am', 'E']).
-acordes_coro('metallica', ['E', 'G', 'F', 'Am']).
-acordes_puente('metallica', ['D', 'C', 'G', 'E']).
-
-% Canción "One"
 acordes_verso('metallica', ['B', 'G', 'D', 'A']).
-acordes_precoro('metallica', ['A', 'G', 'B', 'E']).
-acordes_coro('metallica', ['D', 'G', 'B', 'A']).
-acordes_puente('metallica', ['F', 'G', 'A', 'E']).
-
- % Canción "Welcome to the Jungle"
 acordes_verso('guns_n_roses', ['D', 'C', 'G', 'F']).
-acordes_precoro('guns_n_roses', ['A', 'C', 'D', 'F']).
-acordes_coro('guns_n_roses', ['D', 'C', 'G', 'F']).
-acordes_puente('guns_n_roses', ['A', 'G', 'C', 'D']).
-
-% Canción "Sweet Child O' Mine"
 acordes_verso('guns_n_roses', ['D', 'C', 'G', 'D']).
-acordes_precoro('guns_n_roses', ['C', 'G', 'D', 'Em']).
-acordes_coro('guns_n_roses', ['C', 'G', 'D', 'Em']).
-acordes_puente('guns_n_roses', ['Em', 'G', 'A', 'D']).
-
-% Canción "Paradise City"
 acordes_verso('guns_n_roses', ['G', 'C', 'D', 'A']).
-acordes_precoro('guns_n_roses', ['A', 'C', 'G', 'D']).
-acordes_coro('guns_n_roses', ['G', 'C', 'D', 'A']).
-acordes_puente('guns_n_roses', ['A', 'C', 'G', 'D']).
-
-% Canción "Nightrain"
 acordes_verso('guns_n_roses', ['G', 'C', 'D', 'A']).
-acordes_precoro('guns_n_roses', ['A', 'C', 'D', 'G']).
-acordes_coro('guns_n_roses', ['G', 'C', 'D', 'A']).
-acordes_puente('guns_n_roses', ['A', 'G', 'C', 'D']).
-
-% Canción "Mr. Brownstone"
 acordes_verso('guns_n_roses', ['A', 'G', 'C', 'D']).
-acordes_precoro('guns_n_roses', ['D', 'C', 'G', 'A']).
-acordes_coro('guns_n_roses', ['A', 'G', 'C', 'D']).
-acordes_puente('guns_n_roses', ['C', 'D', 'G', 'A']).
-
-% Canción "Noviembre Sin Ti"
 acordes_verso('reik', ['C', 'G', 'Am', 'F']).
-acordes_precoro('reik', ['F', 'G', 'C', 'Am']).
-acordes_coro('reik', ['C', 'G', 'Am', 'F']).
-acordes_puente('reik', ['Am', 'F', 'G', 'C']).
-
-% Canción "Yo Quisiera"
 acordes_verso('reik', ['G', 'D', 'Em', 'C']).
-acordes_precoro('reik', ['Em', 'C', 'G', 'D']).
-acordes_coro('reik', ['G', 'D', 'Em', 'C']).
-acordes_puente('reik', ['C', 'D', 'Em', 'G']).
-
-% Canción "Invierno"
 acordes_verso('reik', ['Em', 'C', 'G', 'D']).
-acordes_precoro('reik', ['C', 'D', 'Em', 'G']).
-acordes_coro('reik', ['Em', 'C', 'G', 'D']).
-acordes_puente('reik', ['Am', 'Em', 'C', 'D']).
-
-% Canción "Que Vida la Mía"
 acordes_verso('reik', ['G', 'D', 'Em', 'C']).
-acordes_precoro('reik', ['Em', 'C', 'G', 'D']).
-acordes_coro('reik', ['G', 'D', 'Em', 'C']).
-acordes_puente('reik', ['C', 'D', 'Em', 'G']).
-
-% Canción "Me Duele Amarte"
 acordes_verso('reik', ['Am', 'F', 'C', 'G']).
-acordes_precoro('reik', ['F', 'G', 'Am', 'C']).
-acordes_coro('reik', ['Am', 'F', 'C', 'G']).
-acordes_puente('reik', ['C', 'Am', 'F', 'G']).
-
-% Canción "Rayando el Sol"
 acordes_verso('mana', ['G', 'D', 'Em', 'C']).
-acordes_precoro('mana', ['C', 'D', 'Em', 'G']).
-acordes_coro('mana', ['G', 'D', 'Em', 'C']).
-acordes_puente('mana', ['Am', 'C', 'G', 'D']).
-
-% Canción "Clavado en un Bar"
 acordes_verso('mana', ['Em', 'G', 'D', 'C']).
-acordes_precoro('mana', ['C', 'D', 'Em', 'G']).
-acordes_coro('mana', ['Em', 'G', 'D', 'C']).
-acordes_puente('mana', ['Am', 'C', 'G', 'D']).
-
-% Canción "Labios Compartidos"
 acordes_verso('mana', ['C', 'G', 'Am', 'F']).
-acordes_precoro('mana', ['F', 'G', 'Am', 'C']).
-acordes_coro('mana', ['C', 'G', 'Am', 'F']).
-acordes_puente('mana', ['Am', 'F', 'G', 'C']).
-
-% Canción "Oye Mi Amor"
 acordes_verso('mana', ['G', 'D', 'Em', 'C']).
-acordes_precoro('mana', ['C', 'D', 'Em', 'G']).
-acordes_coro('mana', ['G', 'D', 'Em', 'C']).
-acordes_puente('mana', ['Am', 'C', 'G', 'D']).
-
-% Canción "Vivir Sin Aire"
 acordes_verso('mana', ['Am', 'F', 'C', 'G']).
+
+acordes_precoro('metallica', ['C', 'D', 'E', 'G']).
+acordes_precoro('metallica', ['D', 'C', 'G', 'E']).
+acordes_precoro('metallica', ['G', 'B', 'Em', 'D']).
+acordes_precoro('metallica', ['C', 'G', 'Am', 'E']).
+acordes_precoro('metallica', ['A', 'G', 'B', 'E']).
+acordes_precoro('guns_n_roses', ['A', 'C', 'D', 'F']).
+acordes_precoro('guns_n_roses', ['C', 'G', 'D', 'Em']).
+acordes_precoro('guns_n_roses', ['A', 'C', 'G', 'D']).
+acordes_precoro('guns_n_roses', ['A', 'C', 'D', 'G']).
+acordes_precoro('guns_n_roses', ['D', 'C', 'G', 'A']).
+acordes_precoro('reik', ['F', 'G', 'C', 'Am']).
+acordes_precoro('reik', ['Em', 'C', 'G', 'D']).
+acordes_precoro('reik', ['C', 'D', 'Em', 'G']).
+acordes_precoro('reik', ['Em', 'C', 'G', 'D']).
+acordes_precoro('reik', ['F', 'G', 'Am', 'C']).
+acordes_precoro('mana', ['C', 'D', 'Em', 'G']).
+acordes_precoro('mana', ['C', 'D', 'Em', 'G']).
 acordes_precoro('mana', ['F', 'G', 'Am', 'C']).
+acordes_precoro('mana', ['C', 'D', 'Em', 'G']).
+acordes_precoro('mana', ['F', 'G', 'Am', 'C']).
+
+acordes_coro('metallica', ['E', 'G', 'F', 'E']).
+acordes_coro('metallica', ['A', 'G', 'F', 'E']).
+acordes_coro('metallica', ['C', 'D', 'Em', 'G']).
+acordes_coro('metallica', ['E', 'G', 'F', 'Am']).
+acordes_coro('metallica', ['D', 'G', 'B', 'A']).
+acordes_coro('guns_n_roses', ['D', 'C', 'G', 'F']).
+acordes_coro('guns_n_roses', ['C', 'G', 'D', 'Em']).
+acordes_coro('guns_n_roses', ['G', 'C', 'D', 'A']).
+acordes_coro('guns_n_roses', ['G', 'C', 'D', 'A']).
+acordes_coro('guns_n_roses', ['A', 'G', 'C', 'D']).
+acordes_coro('reik', ['C', 'G', 'Am', 'F']).
+acordes_coro('reik', ['G', 'D', 'Em', 'C']).
+acordes_coro('reik', ['Em', 'C', 'G', 'D']).
+acordes_coro('reik', ['G', 'D', 'Em', 'C']).
+acordes_coro('reik', ['Am', 'F', 'C', 'G']).
+acordes_coro('mana', ['G', 'D', 'Em', 'C']).
+acordes_coro('mana', ['Em', 'G', 'D', 'C']).
+acordes_coro('mana', ['C', 'G', 'Am', 'F']).
+acordes_coro('mana', ['G', 'D', 'Em', 'C']).
 acordes_coro('mana', ['Am', 'F', 'C', 'G']).
+
+acordes_puente('metallica', ['D', 'E', 'F', 'G']).
+acordes_puente('metallica', ['B', 'C', 'D', 'E']).
+acordes_puente('metallica', ['C', 'G', 'D', 'Em']).
+acordes_puente('metallica', ['D', 'C', 'G', 'E']).
+acordes_puente('metallica', ['F', 'G', 'A', 'E']).
+acordes_puente('guns_n_roses', ['A', 'G', 'C', 'D']).
+acordes_puente('guns_n_roses', ['Em', 'G', 'A', 'D']).
+acordes_puente('guns_n_roses', ['A', 'C', 'G', 'D']).
+acordes_puente('guns_n_roses', ['A', 'G', 'C', 'D']).
+acordes_puente('guns_n_roses', ['C', 'D', 'G', 'A']).
+acordes_puente('reik', ['Am', 'F', 'G', 'C']).
+acordes_puente('reik', ['C', 'D', 'Em', 'G']).
+acordes_puente('reik', ['Am', 'Em', 'C', 'D']).
+acordes_puente('reik', ['C', 'D', 'Em', 'G']).
+acordes_puente('reik', ['C', 'Am', 'F', 'G']).
+acordes_puente('mana', ['Am', 'C', 'G', 'D']).
+acordes_puente('mana', ['Am', 'C', 'G', 'D']).
+acordes_puente('mana', ['Am', 'F', 'G', 'C']).
+acordes_puente('mana', ['Am', 'C', 'G', 'D']).
 acordes_puente('mana', ['C', 'Am', 'F', 'G']).
