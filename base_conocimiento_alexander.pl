b9bfff8e218896c2938cad006e8bce6741c7c16b
Alexander-Guacan <adguacan@espe.edu.ec>
2024-06-17 21:50:17 -0500

    Group facts by music section

--- base_conocimiento_alexander.pl
+++ base_conocimiento_alexander.pl
@@ -1,119 +1,84 @@
-% Canción "Rust in Peace... Polaris"
 acordes_verso('megadeth', ['A', 'E', 'D', 'G']).
-acordes_precoro('megadeth', ['F#m', 'Bm', 'G', 'A']).
-acordes_coro('megadeth', ['D', 'A', 'E', 'G']).
-acordes_puente('megadeth', ['Em', 'G', 'D', 'C']).
-
-% Canción "Holy Wars"
 acordes_verso('megadeth', ['E', 'A', 'D', 'G']).
-acordes_precoro('megadeth', ['Em', 'C', 'G', 'D']).
-acordes_coro('megadeth', ['A', 'E', 'F#m', 'D']).
-acordes_puente('megadeth', ['Bm', 'C', 'G', 'A']).
-
-% Canción "Tornado of Souls"
 acordes_verso('megadeth', ['D', 'A', 'G', 'Bm']).
-acordes_precoro('megadeth', ['Em', 'G', 'D', 'A']).
-acordes_coro('megadeth', ['Bm', 'A', 'G', 'D']).
-acordes_puente('megadeth', ['Em', 'G', 'D', 'A']).
-
-% Canción "Hangar 18"
 acordes_verso('megadeth', ['Em', 'A', 'G', 'D']).
-acordes_precoro('megadeth', ['C', 'D', 'G', 'Bm']).
-acordes_coro('megadeth', ['Em', 'G', 'D', 'A']).
-acordes_puente('megadeth', ['Bm', 'A', 'G', 'D']).
-
-% Canción "Sweating Bullets"
 acordes_verso('megadeth', ['E', 'G', 'A', 'D']).
-acordes_precoro('megadeth', ['C', 'G', 'D', 'A']).
-acordes_coro('megadeth', ['E', 'A', 'D', 'G']).
-acordes_puente('megadeth', ['C', 'G', 'D', 'A']).
-
-% Canción "Hoy que no estás"
 acordes_verso('juan fernando velasco', ['Am', 'G', 'F', 'C']).
-acordes_precoro('juan fernando velasco', ['Dm', 'G', 'C', 'F']).
-acordes_coro('juan fernando velasco', ['Am', 'G', 'F', 'C']).
-acordes_puente('juan fernando velasco', ['Dm', 'G', 'C', 'F']).
-
-% Canción "Chao Lola"
 acordes_verso('juan fernando velasco', ['C', 'G', 'Am', 'F']).
-acordes_precoro('juan fernando velasco', ['Dm', 'G', 'C', 'Am']).
-acordes_coro('juan fernando velasco', ['F', 'G', 'C', 'Am']).
-acordes_puente('juan fernando velasco', ['Dm', 'G', 'C', 'Am']).
-
-% Canción "Dicen"
 acordes_verso('juan fernando velasco', ['G', 'D', 'Em', 'C']).
-acordes_precoro('juan fernando velasco', ['Am', 'D', 'G', 'C']).
-acordes_coro('juan fernando velasco', ['G', 'D', 'Em', 'C']).
-acordes_puente('juan fernando velasco', ['Am', 'D', 'G', 'C']).
-
-% Canción "Ángel de Luz"
 acordes_verso('juan fernando velasco', ['D', 'A', 'Bm', 'G']).
-acordes_precoro('juan fernando velasco', ['Em', 'A', 'D', 'G']).
-acordes_coro('juan fernando velasco', ['D', 'A', 'Bm', 'G']).
-acordes_puente('juan fernando velasco', ['Em', 'A', 'D', 'G']).
-
-% Canción "Dame un instante"
 acordes_verso('juan fernando velasco', ['G', 'D', 'Em', 'C']).
-acordes_precoro('juan fernando velasco', ['Am', 'D', 'G', 'C']).
-acordes_coro('juan fernando velasco', ['G', 'D', 'Em', 'C']).
-acordes_puente('juan fernando velasco', ['Am', 'D', 'G', 'C']).
-
-% Canción "No me destruyas"
 acordes_verso('zoe', ['G', 'D', 'Em', 'C']).
-acordes_precoro('zoe', ['Am', 'C', 'G', 'D']).
-acordes_coro('zoe', ['G', 'D', 'Em', 'C']).
-acordes_puente('zoe', ['Am', 'C', 'G', 'D']).
-
-% Canción "Nunca"
 acordes_verso('zoe', ['Em', 'C', 'G', 'D']).
-acordes_precoro('zoe', ['Am', 'C', 'G', 'D']).
-acordes_coro('zoe', ['Em', 'C', 'G', 'D']).
-acordes_puente('zoe', ['Am', 'C', 'G', 'D']).
-
-% Canción "Paula"
 acordes_verso('zoe', ['D', 'A', 'Bm', 'G']).
-acordes_precoro('zoe', ['Em', 'A', 'D', 'G']).
-acordes_coro('zoe', ['D', 'A', 'Bm', 'G']).
-acordes_puente('zoe', ['Em', 'A', 'D', 'G']).
-
-% Canción "Poli Love"
 acordes_verso('zoe', ['G', 'D', 'Em', 'C']).
-acordes_precoro('zoe', ['Am', 'C', 'G', 'D']).
-acordes_coro('zoe', ['G', 'D', 'Em', 'C']).
-acordes_puente('zoe', ['Am', 'C', 'G', 'D']).
-
-% Canción "Sombras"
 acordes_verso('zoe', ['C', 'G', 'Am', 'F']).
-acordes_precoro('zoe', ['Dm', 'G', 'C', 'Am']).
-acordes_coro('zoe', ['F', 'G', 'C', 'Am']).
-acordes_puente('zoe', ['Dm', 'G', 'C', 'Am']).
-
-% Canción "Lento"
 acordes_verso('julieta venegas', ['Am', 'F', 'C', 'G']).
-acordes_precoro('julieta venegas', ['Dm', 'G', 'C', 'Am']).
-acordes_coro('julieta venegas', ['F', 'C', 'G', 'Am']).
-acordes_puente('julieta venegas', ['Dm', 'G', 'C', 'Am']).
-
-% Canción "Andar conmigo"
 acordes_verso('julieta venegas', ['G', 'D', 'Em', 'C']).
-acordes_precoro('julieta venegas', ['Am', 'D', 'G', 'C']).
-acordes_coro('julieta venegas', ['G', 'D', 'Em', 'C']).
-acordes_puente('julieta venegas', ['Am', 'D', 'G', 'C']).
-
-% Canción "Algo está cambiando"
 acordes_verso('julieta venegas', ['C', 'G', 'Am', 'F']).
-acordes_precoro('julieta venegas', ['Dm', 'G', 'C', 'Am']).
-acordes_coro('julieta venegas', ['F', 'G', 'C', 'Am']).
-acordes_puente('julieta venegas', ['Dm', 'G', 'C', 'Am']).
-
-% Canción "Mala memoria"
 acordes_verso('julieta venegas', ['D', 'A', 'Bm', 'G']).
-acordes_precoro('julieta venegas', ['Em', 'A', 'D', 'G']).
-acordes_coro('julieta venegas', ['D', 'A', 'Bm', 'G']).
-acordes_puente('julieta venegas', ['Em', 'A', 'D', 'G']).
-
-% Canción "Alguien"
 acordes_verso('julieta venegas', ['Am', 'G', 'F', 'C']).
+
+acordes_precoro('megadeth', ['F#m', 'Bm', 'G', 'A']).
+acordes_precoro('megadeth', ['Em', 'C', 'G', 'D']).
+acordes_precoro('megadeth', ['Em', 'G', 'D', 'A']).
+acordes_precoro('megadeth', ['C', 'D', 'G', 'Bm']).
+acordes_precoro('megadeth', ['C', 'G', 'D', 'A']).
+acordes_precoro('juan fernando velasco', ['Dm', 'G', 'C', 'F']).
+acordes_precoro('juan fernando velasco', ['Dm', 'G', 'C', 'Am']).
+acordes_precoro('juan fernando velasco', ['Am', 'D', 'G', 'C']).
+acordes_precoro('juan fernando velasco', ['Em', 'A', 'D', 'G']).
+acordes_precoro('juan fernando velasco', ['Am', 'D', 'G', 'C']).
+acordes_precoro('zoe', ['Am', 'C', 'G', 'D']).
+acordes_precoro('zoe', ['Am', 'C', 'G', 'D']).
+acordes_precoro('zoe', ['Em', 'A', 'D', 'G']).
+acordes_precoro('zoe', ['Am', 'C', 'G', 'D']).
+acordes_precoro('zoe', ['Dm', 'G', 'C', 'Am']).
+acordes_precoro('julieta venegas', ['Dm', 'G', 'C', 'Am']).
+acordes_precoro('julieta venegas', ['Am', 'D', 'G', 'C']).
+acordes_precoro('julieta venegas', ['Dm', 'G', 'C', 'Am']).
+acordes_precoro('julieta venegas', ['Em', 'A', 'D', 'G']).
 acordes_precoro('julieta venegas', ['Dm', 'G', 'C', 'F']).
+
+acordes_coro('megadeth', ['D', 'A', 'E', 'G']).
+acordes_coro('megadeth', ['A', 'E', 'F#m', 'D']).
+acordes_coro('megadeth', ['Bm', 'A', 'G', 'D']).
+acordes_coro('megadeth', ['Em', 'G', 'D', 'A']).
+acordes_coro('megadeth', ['E', 'A', 'D', 'G']).
+acordes_coro('juan fernando velasco', ['Am', 'G', 'F', 'C']).
+acordes_coro('juan fernando velasco', ['F', 'G', 'C', 'Am']).
+acordes_coro('juan fernando velasco', ['G', 'D', 'Em', 'C']).
+acordes_coro('juan fernando velasco', ['D', 'A', 'Bm', 'G']).
+acordes_coro('juan fernando velasco', ['G', 'D', 'Em', 'C']).
+acordes_coro('zoe', ['G', 'D', 'Em', 'C']).
+acordes_coro('zoe', ['Em', 'C', 'G', 'D']).
+acordes_coro('zoe', ['D', 'A', 'Bm', 'G']).
+acordes_coro('zoe', ['G', 'D', 'Em', 'C']).
+acordes_coro('zoe', ['F', 'G', 'C', 'Am']).
+acordes_coro('julieta venegas', ['F', 'C', 'G', 'Am']).
+acordes_coro('julieta venegas', ['G', 'D', 'Em', 'C']).
+acordes_coro('julieta venegas', ['F', 'G', 'C', 'Am']).
+acordes_coro('julieta venegas', ['D', 'A', 'Bm', 'G']).
 acordes_coro('julieta venegas', ['Am', 'G', 'F', 'C']).
+
+acordes_puente('megadeth', ['Em', 'G', 'D', 'C']).
+acordes_puente('megadeth', ['Bm', 'C', 'G', 'A']).
+acordes_puente('megadeth', ['Em', 'G', 'D', 'A']).
+acordes_puente('megadeth', ['Bm', 'A', 'G', 'D']).
+acordes_puente('megadeth', ['C', 'G', 'D', 'A']).
+acordes_puente('juan fernando velasco', ['Dm', 'G', 'C', 'F']).
+acordes_puente('juan fernando velasco', ['Dm', 'G', 'C', 'Am']).
+acordes_puente('juan fernando velasco', ['Am', 'D', 'G', 'C']).
+acordes_puente('juan fernando velasco', ['Em', 'A', 'D', 'G']).
+acordes_puente('juan fernando velasco', ['Am', 'D', 'G', 'C']).
+acordes_puente('zoe', ['Am', 'C', 'G', 'D']).
+acordes_puente('zoe', ['Am', 'C', 'G', 'D']).
+acordes_puente('zoe', ['Em', 'A', 'D', 'G']).
+acordes_puente('zoe', ['Am', 'C', 'G', 'D']).
+acordes_puente('zoe', ['Dm', 'G', 'C', 'Am']).
+acordes_puente('julieta venegas', ['Dm', 'G', 'C', 'Am']).
+acordes_puente('julieta venegas', ['Am', 'D', 'G', 'C']).
+acordes_puente('julieta venegas', ['Dm', 'G', 'C', 'Am']).
+acordes_puente('julieta venegas', ['Em', 'A', 'D', 'G']).
 acordes_puente('julieta venegas', ['Dm', 'G', 'C', 'F']).
+
